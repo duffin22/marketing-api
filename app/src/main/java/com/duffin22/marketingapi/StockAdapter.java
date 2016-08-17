7b659e0d06bb111704ff51957b23f843469a12c0
Baron-Severin <severinrudie@gmail.com>
2016-08-17 14:12:25 -0700

    added several dependencies to gradle

--- app/src/main/java/com/duffin22/marketingapi/StockAdapter.java
+++ app/src/main/java/com/duffin22/marketingapi/StockAdapter.java
@@ -1,5 +1,12 @@
 package com.duffin22.marketingapi;
 
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
 /**
  * Created by erikrudie on 8/17/16.
  */
@@ -11,13 +18,11 @@ public class StockAdapter extends
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
         public TextView VIEW;
-        public Button VIEW;
 
         public ViewHolder(View itemView) {
             super(itemView);
 
-            VIEW = (TextView) itemView.findViewById(R.id.contact_name);
-            VIEW = (Button) itemView.findViewById(R.id.message_button);
+//            VIEW = (TextView) itemView.findViewById(R.id.contact_name);
         }
     }
 
@@ -38,7 +43,7 @@ public class StockAdapter extends
         Context context = parent.getContext();
         LayoutInflater inflater = LayoutInflater.from(context);
 
-        View view = inflater.inflate(R.layout.RECYCLER_ITEM, parent, false);
+        View view = inflater.inflate(R.layout.recycler_item, parent, false);
 
         ViewHolder viewHolder = new ViewHolder(view);
         return viewHolder;
@@ -50,8 +55,6 @@ public class StockAdapter extends
 
         TextView textView = viewHolder.nameTextView;
         textView.setText(model.getName());
-        Button button = viewHolder.messageButton;
-        button.setText("Message");
     }
 
     // Returns the total count of items in the list
