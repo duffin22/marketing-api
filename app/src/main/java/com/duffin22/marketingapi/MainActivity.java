34958d4c67ecdfdd102a0d7b23f170d82cb972e6
Matthew Duffin <matthewtduffin@gmail.com>
2016-08-17 13:44:13 -0700

    initial commit

---
+++ app/src/main/java/com/duffin22/marketingapi/MainActivity.java
@@ -0,0 +1,13 @@
+package com.duffin22.marketingapi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
