a5db723fb8bd79ba350a42846a6cfdfd67d3b8c3
Matthew Duffin <matthewtduffin@gmail.com>
2016-08-17 13:46:52 -0700

    gitignore changed

--- app/src/main/java/com/duffin22/marketingapi/MainActivity.java
+++ app/src/main/java/com/duffin22/marketingapi/MainActivity.java
@@ -10,4 +10,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    OkHttpClient client = new OkHttpClient();
+
+    String run(String url) throws IOException {
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        Response response = client.newCall(request).execute();
+        return response.body().string();
+    }
 }
